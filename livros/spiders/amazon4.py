7f6b91715bd7f3c6cb0bb7436dc5f06854bb7414
Daniel Fernando Catto <Danielcatto@hotmail.com>
2018-01-04 08:09:21 -0200

    Update amazon4.py

--- livros/spiders/amazon4.py
+++ livros/spiders/amazon4.py
@@ -31,10 +31,10 @@ class QuotesSpider(scrapy.Spider):
 
 
     def comprar(self,preco):
-        consusmer_key = 'yT57HTvtM7drPqQ1fVMtbdsGJ'
-        consusmer_secret = 'AYke23tMy9QNMuWpVMD8UTu7MiP8VL2Aee0j7KT8HuEV88uSqZ'
-        token_key = '50165680-o7CcGmvB9XmSEdDkWGNNnjUEUNsBXD4vErRMG22k1'
-        token_secret = 'zRuIUYEuJfiV7pXQPWkzYgy1a5KODWseIGSoC7iGHJGkU'
+        consusmer_key = ''
+        consusmer_secret = ''
+        token_key = ''
+        token_secret = ''
 
         consumer = oauth2.Consumer(consusmer_key, consusmer_secret)
         token = oauth2.Token(token_key, token_secret)
