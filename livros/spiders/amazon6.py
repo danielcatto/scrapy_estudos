aad7f871ec610f8f7ce125d6551f2a5ba6988cf6
Daniel Fernando Catto <Danielcatto@hotmail.com>
2018-01-04 08:10:49 -0200

    Update amazon6.py

--- livros/spiders/amazon6.py
+++ livros/spiders/amazon6.py
@@ -74,10 +74,10 @@ class QuotesSpider(scrapy.Spider):
 
 
     def twittar(self, produto, preco, preco_comparativo, url, desconto):
-        consusmer_key = 'yT57HTvtM7drPqQ1fVMtbdsGJ'
-        consusmer_secret = 'AYke23tMy9QNMuWpVMD8UTu7MiP8VL2Aee0j7KT8HuEV88uSqZ'
-        token_key = '50165680-o7CcGmvB9XmSEdDkWGNNnjUEUNsBXD4vErRMG22k1'
-        token_secret = 'zRuIUYEuJfiV7pXQPWkzYgy1a5KODWseIGSoC7iGHJGkU'
+        consusmer_key = ''
+        consusmer_secret = ''
+        token_key = '-'
+        token_secret = ''
         consumer = oauth2.Consumer(consusmer_key, consusmer_secret)
         token = oauth2.Token(token_key, token_secret)
         cliente = oauth2.Client(consumer, token)
