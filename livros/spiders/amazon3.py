80c7309525e3cd2cebc75b0c846b12d1d3521aa3
Daniel Fernando Catto <Danielcatto@hotmail.com>
2018-01-04 08:08:40 -0200

    Update amazon3.py

--- livros/spiders/amazon3.py
+++ livros/spiders/amazon3.py
@@ -37,10 +37,10 @@ class QuotesSpider(scrapy.Spider):
         print('\n\n\n')
 
     def twittar(self,preco):
-        consusmer_key = 'yT57HTvtM7drPqQ1fVMtbdsGJ'
-        consusmer_secret = 'AYke23tMy9QNMuWpVMD8UTu7MiP8VL2Aee0j7KT8HuEV88uSqZ'
-        token_key = '50165680-o7CcGmvB9XmSEdDkWGNNnjUEUNsBXD4vErRMG22k1'
-        token_secret = 'zRuIUYEuJfiV7pXQPWkzYgy1a5KODWseIGSoC7iGHJGkU'
+        consusmer_key = ''
+        consusmer_secret = ''
+        token_key = ''
+        token_secret = ''
 
         consumer = oauth2.Consumer(consusmer_key, consusmer_secret)
         token = oauth2.Token(token_key, token_secret)
